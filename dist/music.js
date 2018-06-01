3e3e6269f13507e828e3f834d95e4030aefaa3c3
heyjude6688 <dribbird@sina.com>
2018-06-01 17:01:38 +0800

    Site updated: 2018-06-01 17:01:37

--- dist/music.js
+++ dist/music.js
@@ -4,34 +4,34 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
-        name: 'Imagine me without you',
-        artist: 'Jaci Velasquez',
-        url: 'http://up.mcyt.net/?down/14401.mp3',
-        cover: 'http://chuantu.biz/t6/322/1527840257x-1404793549.jpg',
-      },
-      {
         name: "Lost In Paradise",
         artist: '王若琳',
         url: 'http://up.mcyt.net/?down/37280.mp3',
-        cover: 'http://chuantu.biz/t6/322/1527839831x-1566638135.jpg',
+        cover: 'http://chuantu.biz/t6/322/1527843571x1822611341.jpg',
+      },
+      {
+        name: 'Imagine me without you',
+        artist: 'Jaci Velasquez',
+        url: 'http://up.mcyt.net/?down/14401.mp3',
+        cover: 'http://chuantu.biz/t6/322/1527843571x1822611341.jpg',
       },
       {
         name: 'Fairy Tale',
         artist: 'Michael Learns',
         url: 'http://up.mcyt.net/?down/35037.mp3',
-        cover: 'http://chuantu.biz/t6/322/1527840403x-1404793549.jpg',
+        cover: 'http://chuantu.biz/t6/322/1527843571x1822611341.jpg',
       },
       {
         name: 'Sunny Day',
         artist: 'Joy Williams ',
         url: 'http://up.mcyt.net/?down/41385.mp3',
-        cover: 'http://chuantu.biz/t6/322/1527840282x-1404793549.jpg',
+        cover: 'http://chuantu.biz/t6/322/1527843571x1822611341.jpg',
       },
       {
         name: 'Nights Changes',
         artist: 'One Direction',
         url: 'http://up.mcyt.net/?down/43934.mp3',
-        cover: 'http://chuantu.biz/t6/322/1527840230x-1404793549.jpg',
+        cover: 'http://chuantu.biz/t6/322/1527843571x1822611341.jpg',
       }
     ]
 });
